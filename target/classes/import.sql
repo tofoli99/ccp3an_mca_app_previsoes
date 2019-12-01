5fa813bddfd0b5886bdecd96bd029159bd327cac
tofoli99 <leoadt@live.com>
2019-12-01 15:24:10 -0300

    Todas as entrgas das aulasadĩcao import.sql

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,33 +1,30 @@
 
-	insert into previsoes (min, max, hum, des, data, hora) values (21.0, 31.0, 73.0, 'alta chance de chuva','14/9/2019', 22.45);
-	insert into dias (semana) values ('Sexta');
-	insert into cidades (nome,latitude, longitude) values ('palmiras', 180.55, 55.27 );
-	
+	insert into previsoes (min, max, hum, des, data, hora) values (21.0, 24.0, 73.0, 'possivel pancada de chuva durante a tarde','14/11/2019', 22.00);
+	insert into dias (semana) values ('Segunda');
+	insert into cidades (nome,latitude, longitude) values ('Osasco', 111.22, 33.44 );	
 	
 	update previsoes set dia = 1 where id = 1;
 	update previsoes set cidade_id = 1 where id = 1;
 	update dias set previsao = 1 where id = 1;
 	
-	
-	insert into previsoes (min, max, hum, des, data, hora) values ( 20.0, 33.0, 90.0, 'alta chance de chuvas fortes','15/9/2019', 16.35);
-	insert into dias (semana) values ('Sábado');
-	insert into cidades (nome,latitude, longitude) values ('Corinthians' , 80.23, 65.98);
-
-	
+	--------------------------------------------------------------------------------------------------------------------------------
+	insert into previsoes (min, max, hum, des, data, hora) values ( 23.0, 233.0, 30.0, 'Tempo seco','15/11/2019', 10.35);
+	insert into dias (semana) values ('Terca');
+	insert into cidades (nome,latitude, longitude) values ('São Paulo' , 99.88, 77.66);	
 	
 	update previsoes set dia = 2 where id = 2;
 	update previsoes set cidade_id = 2 where id = 2;
 	update dias set previsao = 2 where id = 2;
-
-	insert into previsoes (min, max, hum, des, data, hora) values (18.0, 31.0, 63.0, 'pouca chance de chuva','16/9/2019', 10.15);
-	insert into dias (semana) values ('Domingo');
-	insert into cidades (nome,latitude, longitude) values ('São camilo', 90.55, 75.88);
+-----------------------------------------------------------------------------------------------------------------------------------
+	insert into previsoes (min, max, hum, des, data, hora) values (10.0, 18.0, 63.0, 'Frio','16/11/2019', 19.15);
+	insert into dias (semana) values ('Quarta');
+	insert into cidades (nome,latitude, longitude) values ('Barueri', 01.23, 45.67);
 
 	
 	update previsoes set dia = 3 where id = 3;
 	update previsoes set cidade_id = 3 where id = 3;
 	update dias set previsao = 3 where id = 3;
 
---adicione um usuário
-insert into usuario (id, login, senha) values (1, 'admin', 'admin');
+	--adicione um usuário
+	insert into usuario (id, login, senha) values (1, 'admin', 'admin');
 
